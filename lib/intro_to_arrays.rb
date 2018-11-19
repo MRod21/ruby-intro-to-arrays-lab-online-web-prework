5c4c77125440156c905585d90ec5091c7e7a9de1
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-19 01:40:45 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,9 +19,9 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  @south_east_asia.last
+  @south_east_asia.first
 end
 
 def last_element_with_array_methods(array)
-  @south_east_asia.first
+  @south_east_asia.last
 end
